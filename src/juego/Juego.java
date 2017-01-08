976c4f767f36c25c113000cf8434ed5457b207bc
Joselu <joseludam2000@gmail.com>
2017-01-08 16:22:48 +0100

    Creada la ventana que alojará el juego

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,12 +1,33 @@
 package juego;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Juego extends Canvas {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	private static final int ANCHO = 800;
+	private static final int ALTO = 600;
+	private static final String NOMBRE = "Juego";
+	private static JFrame ventana;
 
+	private Juego() {
+		setPreferredSize(new Dimension(ANCHO, ALTO));
+
+		ventana = new JFrame(NOMBRE);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setResizable(false);
+		ventana.setLayout(new BorderLayout());
+		ventana.add(this, BorderLayout.CENTER);
+		ventana.pack();
+		ventana.setLocationRelativeTo(null);
+		ventana.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Juego juego = new Juego();
+	}
 }
