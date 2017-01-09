11a438105c41eff081aceda0a40caf5c8f29ee6f
Joselu <joseludam2000@gmail.com>
2017-01-09 20:38:46 +0100

    Añadido icono a la ventana del programa

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 import control.Teclado;
@@ -30,6 +31,7 @@ public class Juego extends Canvas implements Runnable {
 	private static Pantalla pantalla;
 	private static BufferedImage imagen = new BufferedImage(ANCHO, ALTO, BufferedImage.TYPE_INT_RGB);
 	private static int[] pixeles = ((DataBufferInt) imagen.getRaster().getDataBuffer()).getData();
+	private static final ImageIcon icono = new ImageIcon(Juego.class.getResource("/icono/ojo.png"));
 
 	private Juego() {
 		setPreferredSize(new Dimension(ANCHO, ALTO));
@@ -40,6 +42,7 @@ public class Juego extends Canvas implements Runnable {
 		ventana = new JFrame(NOMBRE);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventana.setResizable(false);
+		ventana.setIconImage(icono.getImage());
 		ventana.setLayout(new BorderLayout());
 		ventana.add(this, BorderLayout.CENTER);
 		ventana.pack();
